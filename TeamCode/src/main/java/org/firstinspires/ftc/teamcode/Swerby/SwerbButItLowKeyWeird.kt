79fa57d558f8a38b014bf39d192b5ec6be67a8b2
MuffinHeadHD <muffinheadhd@gmail.com>
2026-04-19 17:46:55 -0400

    Swerb conversion

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerby/SwerbButItLowKeyWeird.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerby/SwerbButItLowKeyWeird.kt
@@ -3,79 +3,105 @@ package org.firstinspires.ftc.teamcode.Swerby
 import com.acmerobotics.dashboard.config.Config
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
-import com.qualcomm.robotcore.hardware.AnalogInput
 import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorEx
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion
+import kotlin.math.abs
 
 @TeleOp
 
 @Config
 class SwerbButItLowKeyWeird {
+        companion object {
+            @JvmField
+            var kP = 0.0
+            @JvmField
+            var kD = 0.0
+        }
+}
 
     @TeleOp(name = "Swerb I think")
     public class Swerby : LinearOpMode() {
         lateinit var one: DcMotor
         lateinit var two: DcMotor
-        lateinit var three: DcMotor
-        lateinit var four: DcMotor
 
-        lateinit var encoderLeft : AnalogInput
-        lateinit var encoderRight: AnalogInput
+
+        lateinit var encoderLeft : DcMotorEx
 
 
         override fun runOpMode() {
             val hwMap: HardwareMap = BlocksOpModeCompanion.opMode.hardwareMap
+
             one = hwMap.get(DcMotor::class.java, "one")
             two = hwMap.get(DcMotor::class.java, "two")
-            three = hwMap.get(DcMotor::class.java, "three")
-            four = hwMap.get(DcMotor::class.java, "four")
 
-            encoderLeft = hwMap.get(AnalogInput::class.java, "encoderLeft")
-            encoderRight = hwMap.get(AnalogInput::class.java, "encoderRight")
+
+            encoderLeft = hwMap.get(DcMotorEx::class.java, "encoderLeft")
+
 
             one.setDirection(DcMotorSimple.Direction.FORWARD)
             two.setDirection(DcMotorSimple.Direction.REVERSE)
-            three.setDirection(DcMotorSimple.Direction.REVERSE)
-            four.setDirection(DcMotorSimple.Direction.REVERSE)
+
 
             one.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
             two.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
-            three.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
-            four.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+
 
             one.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
             two.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
-            three.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
-            four.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
+
+
+            // All the random variables go here:
+            var leftPodAngle = encoderLeft.currentPosition
+
 
 
             waitForStart();
 
+            fun wrapTo180(angle: Double): Double {
+                var Angle_Of_360 = angle % 360.0
+                if (Angle_Of_360 > 180.0) Angle_Of_360 -= 360.0
+                if (Angle_Of_360 < -180.0) Angle_Of_360 += 360.0
+                return Angle_Of_360
+            }
+
+            fun angleError(target: Double, current: Double): Double {
+                return wrapTo180(target - current)
+            }
+
+            val targetAngle = wrapTo180(angleGoal)
+
+            val noSteeringInput =
+                Math.abs(gamepad1.right_stick_x) < 0.05 && abs(gamepad1.right_stick_y) < 0.05
+
+            val targetAngle = if (noSteeringInput) {
+                0.0   // home position
+            } else {
+                calculateAngleFromController()
+            }.let { wrapTo180(it) }
+
+            val currentAngle = wrapTo180(leftPodAngle)
+            val error = angleError(targetAngle, currentAngle)
+
+            var lastError = error
+            val steeringPower = SwerbButItLowKeyWeird.kP * error + SwerbButItLowKeyWeird.kD * (error - lastError)
+
+
+
             while (opModeIsActive()) {
-                var leftPodVolts = encoderLeft.getVoltage()
-                var leftPodAngle = (leftPodVolts * 0.53571)
-
-                var rightPodVolts = encoderRight.getVoltage()
-                var rightPodAngle = ( rightPodVolts * 0.53571)
-
 
                 var drive = -gamepad1.left_stick_y
                 var turn = -gamepad1.right_stick_x
 
                 var OnePower = (drive + turn) * 1.0
                 var TwoPower = (drive - turn) * 1.0
-                var ThreePower = (drive - turn) * 1.0
-                var FourPower = (drive + turn) * 1.0
 
                 one.setPower(OnePower)
                 two.setPower(TwoPower)
-                three.setPower(ThreePower)
-                four.setPower(FourPower)
             }
 
         }
 
     }
-}
